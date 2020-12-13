67c3849505cc05208135a24e58a2cd321328599c
VeArnold <56921374+VeArnold@users.noreply.github.com>
2020-12-13 18:03:28 +0200

    Add like, dislike, post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -25,18 +25,42 @@ router.get('/', authorize, (request, response) => {
 router.post('/', authorize,  (request, response) => {
 
     // Endpoint to create a new post
+    const media = {
+        type: request.body.media.type,
+        url: request.body.media.url
+    }
+
+    const post = {
+        media,
+        userId: request.currentUser.id,
+        text: request.body.text,
+    }
+
+    PostModel.create(post, () => {
+        response.status(201).json()
+    })
 
 });
-
 
 router.put('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to like a post
+    const user = request.currentUser.id;
+    const post = request.params.postId;
+    PostModel.like(user, post, () => {
+        response.status(201).json()
+    });
+
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
+    const user = request.currentUser.id;
+    const post = request.params.postId;
+    PostModel.unlike(user, post, () => {
+        response.status(201).json()
+    });
 
 });
 
